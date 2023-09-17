d087760fa5bbf1b6dbd91721e908371c52827bf8
alexviktoria <tori-r@mail.ru>
2023-09-17 20:04:02 +0200

    us_11-03-04

---
+++ pages/bannerButtons.js
@@ -0,0 +1,77 @@
+const { test, expect } = require("@playwright/test");
+
+class BannerBtn {
+    constructor(page) {
+        this.page = page;
+        this.StartTradingBtnOnMainBanner = page.locator('a.btn--darkText[data-type="top_banner_btn"]')
+        this.TryDemoBtnOnMainBanner = page.locator('.btn--emptyblack')
+        this.TradeBtnOnWidgetMostTraded = page.$$('.mostTraded__btn')
+        this.SellBtnOnBanner = page.locator('a.button-main.sell')
+        this.BuyBtnOnBanner = page.locator('a.button-main.buy')
+        // Footer
+        this.DownloadOnAppStoreBtn = page.locator('[data-type = "banner_capital_ios"]')
+        this.DownloadOnGooglePlayLink = page.locator('[data-type = "banner_capital_google"]')
+        this.ExploreWebPlatformLink = page.locator('[data-type="banner_capital_platform"]')
+        this.CreateAndVerifyBtn = page.locator('i.regSteps__item.js_signup')
+        this.LogoAppStore = page.locator('a.globalnav-link-apple')
+        this.IconCapital = page.locator('picture#ember3')
+        this.ProductNameOnAppstore = page.getByRole('heading', { name: 'Capital.com: Trading & Finance 17+' })
+        this.LinkCapitalComOnAppstore = page.getByRole('heading', { name: 'Capital Com SV Investments Limited' })
+        this.LogoGooglePlay = page.locator('a.f0UV3d')
+        this.ProdactNameOnGoogleplay = page.locator('h1.Fd93Bb.F5UCq.p5VxAd')
+        this.LinkCapitalComOnGoogleplay = page.locator('div.Vbfug.auoIOc')
+    }
+
+    // Methods
+
+    async clickStartTradingBtnOnMainBanner() {
+        await this.StartTradingBtnOnMainBanner.click()
+    }
+
+    async clickTryDemoBtnOnMainBanner() {
+        await this.TryDemoBtnOnMainBanner.click()
+    }
+    
+    async clickTradeBtnOnWidgetMostTraded() {
+        try {
+            await this.TradeBtnOnWidgetMostTraded.toBeVisible();
+            await this.TradeBtnOnWidgetMostTraded.click();
+        } catch (error) {
+            console.log(`For test on FCA license the button [Trade] doen't displayed `)
+            throw new Error();
+        }
+    }
+
+    async clickDownloadOnAppStoreBtn() {
+        await this.DownloadOnAppStoreBtn.click();
+    }
+
+    async clickDownloadOnGooglePlayLink() {
+        await this.DownloadOnGooglePlayLink.click();
+        await this.page.waitForNavigation(); // ожидание загрузки новой страницы
+        const currentUrl = await this.page.url();
+        if (currentUrl === 'https://apps.apple.com/IE/app/id1230088754?mt=8') {
+            console.log('The link to App Store instead of Google Play');
+            throw new Error();
+        }
+    }
+
+
+    async clickExploreWebPlatformLink() {
+        await this.ExploreWebPlatformLink.click();
+        
+    }
+
+    async clickCreateAndVerifyBtn() {
+        await this.CreateAndVerifyBtn.click();
+    }
+
+    async clickSellBtnOnBanner() {
+        await this.SellBtnOnBanner.click();
+    }
+
+    async clickBuyBtnOnBanner() {
+        await this.BuyBtnOnBanner.click();
+    }
+}
+module.exports = { BannerBtn };
---
+++ pages/linksFile.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+class LinksFile {
+    constructor(page, header, login) {
+        this.page = page;
+        this.header = header;
+        this.login = login;
+    }
+
+    async linksSaveToFile_UnReg(page, bannerBtn, signup) {
+        const links = await page.$$eval('a[data-type="sidebar_deeplink"]', (elements) => elements.map((el) => el.href));
+        if (links.length === 0) {
+            console.log("There are no links on this page and testing of the second level is impossible");
+        } else {
+            console.log("links", links);
+        }
+        // запись элементов массива "links" в файл "links.txt" с использованием метода "writeFileSync" из модуля "fs"
+        await fs.writeFileSync('links.txt', links.join('\n'));
+        const fileContent = fs.readFileSync('links.txt', 'utf-8');
+        const linksFromFile = fileContent.split('\n').filter((link) => link !== '');
+        const randomLinks = await getRandomElements(linksFromFile, 3);
+        for (let i = 0; i < randomLinks.length; i++) {
+            await page.goto(randomLinks[i]);
+            await bannerBtn.clickStartTradingBtnOnMainBanner();
+            await signup.signUpFormIsVisible();
+
+            if (links.includes(randomLinks[i])) {
+                console.log(`Testing on the '${randomLinks[i]}' link was successfully completed`);
+            } else {
+                console.log(`Testing on the '${randomLinks[i]}' link was failed`);
+            }
+        }
+    }
+
+    async linksSaveToFile_UnAuth(page, bannerBtn, login) {
+        const links = await page.$$eval('a[data-type="sidebar_deeplink"]', (elements) => elements.map((el) => el.href));
+        if (links.length === 0) {
+            console.log("There are no links on this page and testing of the second level is impossible");
+        } else {
+            console.log("links", links);
+        }
+        // запись элементов массива "links" в файл "links.txt" с использованием метода "writeFileSync" из модуля "fs"
+        await fs.writeFileSync('links.txt', links.join('\n'));
+        const fileContent = fs.readFileSync('links.txt', 'utf-8');
+        const linksFromFile = fileContent.split('\n').filter((link) => link !== '');
+        const randomLinks = await getRandomElements(linksFromFile, 3);
+        for (let i = 0; i < randomLinks.length; i++) {
+            await page.goto(randomLinks[i]);
+            // await bannerBtn.clickStartTradingBtnOnMainBanner();
+            // await login.LoginFotmIsVisible();
+
+            if (links.includes(randomLinks[i])) {
+                console.log(`Testing on the '${randomLinks[i]}' link was successfully completed`);
+            } else {
+                console.log(`Testing on the '${randomLinks[i]}' link was failed`);
+            }
+        }
+    }
+}
+
+    async function getRandomElements(array, count) {
+    const randomized = array.slice();
+
+    for (let i = randomized.length - 1; i > 1; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [randomized[i], randomized[j]] = [randomized[j], randomized[i]];
+    }
+
+    return randomized.slice(0, count);
+}
+
+
+module.exports = { LinksFile };
