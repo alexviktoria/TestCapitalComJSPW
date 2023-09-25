d21b97d1c9025e1667047db7c1d6384501c1d465
Alexeeva_Viktoria <119632454+alexviktoria@users.noreply.github.com>
2023-09-25 20:22:34 +0200

    us_11-03-04(05) de,de (#24)

--- e2e/Education/US_11-03-04.spec.js
+++ e2e/Education/US_11-03-04.spec.js
@@ -14,7 +14,7 @@ let login;
 let pretest
 
 
-const language = "ro"
+const language = "de"
 
 test.describe("US_11-03-04_Education > Menu item [Position Trading]  on UnReg Role", () => {
     test.beforeAll(async ({ browser }) => {
