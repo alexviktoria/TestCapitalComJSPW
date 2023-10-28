67a33cc205ac9e45912f1be1b3b0422ff7cf30e6
alexviktoria <tori-r@mail.ru>
2023-10-28 15:10:59 +0200

    us-ar,ae

--- pages/header.js
+++ pages/header.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
-const language = "Polski"
-const country = "Poland"
+const language = "العَرَبِيَّة"
+const country = "United Arab Emirates"
 exports.Header = class Header {
     constructor(page) {
         this.page = page;
