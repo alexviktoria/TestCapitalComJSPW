a0f0da10dee634f49dfc2066c10023debc86532b
alexviktoria <tori-r@mail.ru>
2023-11-09 16:37:35 +0200

    refactor: change language and license

--- e2e/Education/US_11-03-05.spec.js
+++ e2e/Education/US_11-03-05.spec.js
@@ -14,7 +14,7 @@ let login;
 let pretest;
 
 const language = 'en';
-const license = 'FCA';
+const license = 'CYSEC';
 
 test.describe('US_11-03-05_Education > Menu item [Swing Trading]  on UnReg Role', () => {
   test.beforeEach(async ({ browser }) => {
